e1c01d713f3bd77216b6e512ae122d2caff06183
Christian Martinez <mar13137@byui.edu>
2019-07-16 09:23:17 -0600

    Added weekly reading practice problems.

--- TrumpTopia/index.js
+++ TrumpTopia/index.js
@@ -12,6 +12,44 @@ function loadQuote() {
 function appendData(data) {
     var x = data.message;
     document.getElementById("myData").innerHTML = x;
-    console.log(x);
+    //console.log(x);
+    }
+    loadKanyeQuote();
+    //loadTaylorQuote();
 }
-  }
+
+function loadKanyeQuote() {
+    fetch('https://api.kanye.rest')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+function appendData(data) {
+    var x = data.quote;
+    document.getElementById("kanyeData").innerHTML = x;
+    }
+}
+
+// https://api.baelor.io/
+
+function loadTaylorQuote() {
+    fetch('https://api.baelor.io/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+function appendData(data) {
+    var x = data.quote;
+    document.getElementById("TaylorData").innerHTML = x;
+    }
+}
